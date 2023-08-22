d44a2b9dad2cb8d16e335733709a5b682cbf20b6
Swarnav Kumar <swarnav786@gmail.com>
2023-08-23 00:28:54 +0530

    Create Question-7.java

---
+++ LAB-7/Question-7.java
@@ -0,0 +1,26 @@
+/*Write a Java program to generate an
+ ArrayIndexOutofBoundsException and handle it using 
+ catch statement.
+*/
+
+import java.util.Scanner;
+public class program8_7 {
+    public static void main(String[] args) {
+        int[] arr = {1,2,3,4,5};
+        System.out.println("The Array is:");
+        for(int i=0;i<arr.length;i++){
+            System.out.print(arr[i]+" ");
+        }
+        Scanner sc = new Scanner(System.in);
+        System.out.print("\nEnter the index to access the array: ");
+        try {
+            int index = sc.nextInt();
+            sc.close();
+            System.out.println(arr[index] + " is at index " + index + " of the array");
+        } 
+        catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println(e);
+        }
+        System.out.println("Rest of the Code"); 
+    }   
+}
